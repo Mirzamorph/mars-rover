e380110cd25d53c53e4f6b5e8beccf93efd1a521
Rustam <mirzamorf@yandex.ru>
2021-04-07 11:44:01 +0300

    refactor with redux

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Grid from './components/grid'
-import {defaultGridSize, defaultRoverDirection, defaultRoverPosition} from './config'
-import Console from './components/console'
 import Rover from './components/rover'
+import Console from './components/console'
+import {defaultGridSize} from './config'
 
-
-// I should have used redux here but i have no time now
-const intialState = {
-    position: defaultRoverPosition,
-    direction: defaultRoverDirection
-}
 
 export default function App() {
-
-    const [pos, setPos] = useState(intialState)
 
     return (
         <>
             <div className="container">
-                <Grid gridCount={defaultGridSize}/>
-                <Rover pos={pos} />
+                <Grid gridCount={defaultGridSize} />
+                <Rover />
             </div>
-            <Console pos={pos} setPos={setPos} />
+            <Console />
         </>
     )
 }
