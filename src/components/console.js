d610dee46071f6e21cd52eb3e0879cb9b6c359c2
Rustam <mirzamorf@yandex.ru>
2021-04-07 12:15:32 +0300

    refactor console component with custom hooks

--- src/components/console.js
+++ src/components/console.js
@@ -1,67 +1,21 @@
 import React, {useState} from 'react'
 import validateCommand from '../services/validateCommand'
-import {commands} from '../config'
-// i know it looks ugly now :(
-export default function Console({pos, setPos}) {
+import useCommand from '../hooks/useCommand'
 
+export default function Console() {
+
+    const runCommand = useCommand()
     const [cmd, setCmd] = useState('')
 
     const handleClick = () => {
         setCmd(validateCommand(cmd))
-
-        cmd.split('').forEach((l, i) => {
-            setTimeout(() => {
-                switch (l) {
-                case commands.forward:
-                    setPos(getNewPosition)
-                    break
-                case commands.left:
-                    setPos(prev => {
-                        return {
-                            direction: prev.direction ? prev.direction - 1 : 3,
-                            position: prev.position
-                        }
-                    })
-                    break
-                case commands.right:
-                    setPos(prev => {
-                        return {
-                            direction: (prev.direction + 1) % 4,
-                            position: prev.position
-                        }
-                    })
-                    break
-                }
-                }, 1000 * i)
-            })
-    }
-
-
-
-    const getNewPosition = ({direction, position}) => {
-        const newPos = {...position}
-
-        switch (direction) {
-            case 0:
-                newPos.x++
-                break
-            case 1:
-                newPos.y++
-                break
-            case 2:
-                newPos.x--
-                break
-            case 3:
-                newPos.y--
-                break
-        }
-        return {direction, position: newPos}
+        runCommand(cmd)
     }
 
     return (
-        <>
+        <div>
             <input type="text" value={cmd} onChange={e => setCmd(e.target.value)}/>
             <button onClick={handleClick}>Run</button>
-        </>
+        </div>
     )
 }
